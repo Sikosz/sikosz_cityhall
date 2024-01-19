83c8765bbfbecb8a0a694c990cb0f55480886b6c
Sikosz <88151230+Sikosz@users.noreply.github.com>
2024-01-19 19:49:57 +0100

    Update config.lua
    added oxtarget and coords

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 config = {
     purchasePrice = 300, -- Ez a kiváltás ára tehát mennyiért szeretnéd kiváltani.
     driverlicense = true, --Ha ez true akkor kötelező hogy letedd a vizsgát a jogosítvány megvásárlásához, ha false akkor nem kötelező    
-    driveritem = 'drivelicense' -- Ide azt írd ami nálad jogosítvány item neve, tehát amit megadtál config.js-ben!
+    driveritem = 'drivelicense', -- Ide azt írd ami nálad jogosítvány item neve, tehát amit megadtál config.js-ben!
+    oxtarget = true, --Ha ez true akkor oxtargetet használsz, ha false akkor 3dtextet
+    coords = vector3(-548.281311, -191.881317, 37.210205), --Kordináta ahol a ped legyen, ha oxtarget = false akkor a 3dtext és a blip
 }
